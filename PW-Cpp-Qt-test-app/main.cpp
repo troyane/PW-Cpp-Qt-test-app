24a9304ec36d47a51581d81d82bbf2a0feeae615
Nazar Gerasymchuk <nazar.gerasymchuk@felgo.com>
2024-02-21 18:14:01 +0200

    Update main.cpp

--- PW-Cpp-Qt-test-app/main.cpp
+++ PW-Cpp-Qt-test-app/main.cpp
@@ -1,11 +1,10 @@
 #include <QCoreApplication>
 #include <QDebug>
 
-
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    qDebug() << "Hi there!";
+    qDebug() << "Hi there! What about linter?";
 
     return a.exec();
 }
