22eb7c2df1d5157e8c590cf63acc9a98513514c5
Nazar <477468+troyane@users.noreply.github.com>
2024-02-21 23:43:21 +0200

    Update main.cpp bad formatting

--- PW-Cpp-Qt-test-app/main.cpp
+++ PW-Cpp-Qt-test-app/main.cpp
@@ -1,7 +1,7 @@
 #include <QCoreApplication>
 #include <QDebug>
 
-int main(int argc, char * argv [])     {
+int main(int argc, char * argv [])          {
     QCoreApplication a(argc, argv);
     qDebug() << "Hi there! What about linter?";
 
