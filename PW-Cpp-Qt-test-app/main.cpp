4b0883544e7e513f29a77dac6c9cbc78d37283df
Nazar Gerasymchuk <nazar.gerasymchuk@felgo.com>
2024-02-22 22:51:46 +0200

    Update app to get more info on build system

--- PW-Cpp-Qt-test-app/main.cpp
+++ PW-Cpp-Qt-test-app/main.cpp
@@ -1,10 +1,20 @@
 #include <QCoreApplication>
 #include <QDebug>
+#include <QSysInfo>
 
 int main(int argc, char* argv[]) {
 
     QCoreApplication a(argc, argv);
-    qDebug() << "Hi there! What about linter?";
+    qInfo() << "- - -";
+    qInfo() << "Hello from Qt console application!";
+    qInfo() << "buildAbi:" << QSysInfo::buildAbi();
+    qInfo() << "buildCpuArchitecture:" << QSysInfo::buildCpuArchitecture();
+    qInfo() << "currentCpuArchitecture:" << QSysInfo::currentCpuArchitecture();
+    qInfo() << "kernelType:" << QSysInfo::kernelType();
+    qInfo() << "kernelVersion:" << QSysInfo::kernelVersion();
+    qInfo() << "machineHostName:" << QSysInfo::machineHostName();
+    qInfo() << "machineUniqueId:" << QSysInfo::machineUniqueId();
+    qInfo() << "- - -";
 
-    return a.exec();
+    return 0;
 }
