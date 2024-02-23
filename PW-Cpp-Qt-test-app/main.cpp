62fa63aca90b03eddf1509dc4d28b4ed11e90d99
Nazar Gerasymchuk <nazar.gerasymchuk@felgo.com>
2024-02-23 11:41:54 +0200

    Update code; Test everywhere

--- PW-Cpp-Qt-test-app/main.cpp
+++ PW-Cpp-Qt-test-app/main.cpp
@@ -1,20 +1,25 @@
 #include <QCoreApplication>
 #include <QDebug>
+#include <QIODevice>
 #include <QSysInfo>
 
 int main(int argc, char* argv[]) {
 
     QCoreApplication a(argc, argv);
-    qInfo() << "- - -";
-    qInfo() << "Hello from Qt console application!";
-    qInfo() << "buildAbi:" << QSysInfo::buildAbi();
-    qInfo() << "buildCpuArchitecture:" << QSysInfo::buildCpuArchitecture();
-    qInfo() << "currentCpuArchitecture:" << QSysInfo::currentCpuArchitecture();
-    qInfo() << "kernelType:" << QSysInfo::kernelType();
-    qInfo() << "kernelVersion:" << QSysInfo::kernelVersion();
-    qInfo() << "machineHostName:" << QSysInfo::machineHostName();
-    qInfo() << "machineUniqueId:" << QSysInfo::machineUniqueId();
-    qInfo() << "- - -";
+    QTextStream(stdout) << "- - -" << Qt::endl;
+    QTextStream(stdout) << "buildAbi: " << QSysInfo::buildAbi() << Qt::endl;
+    QTextStream(stdout) << "buildCpuArchitecture: "
+                        << QSysInfo::buildCpuArchitecture() << Qt::endl;
+    QTextStream(stdout) << "currentCpuArchitecture: "
+                        << QSysInfo::currentCpuArchitecture() << Qt::endl;
+    QTextStream(stdout) << "kernelType: " << QSysInfo::kernelType() << Qt::endl;
+    QTextStream(stdout) << "kernelVersion: " << QSysInfo::kernelVersion()
+                        << Qt::endl;
+    QTextStream(stdout) << "machineHostName: " << QSysInfo::machineHostName()
+                        << Qt::endl;
+    QTextStream(stdout) << "machineUniqueId: " << QSysInfo::machineUniqueId()
+                        << Qt::endl;
+    QTextStream(stdout) << "- - -" << Qt::endl;
 
     return 0;
 }
